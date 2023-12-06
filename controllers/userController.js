6c5c3798765d9992c7645e9183c1795d3b460a1f
Jack Spence <jackspence123456@gmail.com>
2023-12-06 21:18:47 +0200

    Added method to delete a user by their _id.

--- controllers/userController.js
+++ controllers/userController.js
@@ -74,4 +74,27 @@ module.exports = {
 			res.status(500).json(err);
 		}
 	},
+
+	//  TODO: Delete any associated friends & thoughts
+	// DELETE to remove user by their _id
+	async deleteUser(req, res) {
+		try {
+			// findOneAndDelete method params
+			const filter = { id: req.params.id };
+			const options = { new: true };
+
+			const userData = await User.findOneAndDelete(filter, options);
+
+			// Checking if the User exists
+			if (!userData) {
+				res.status(400).json({
+					message: "I can't seem to find the User you're trying to delete!",
+				});
+				return;
+			}
+			res.status(200).json(userData);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
 };
