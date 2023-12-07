6cafd7e0690bf17335054865f959e2779701a075
Jack Spence <jackspence123456@gmail.com>
2023-12-07 12:12:24 +0200

    Added method to remove a friend from a user's friend list.

--- controllers/friendController.js
+++ controllers/friendController.js
@@ -14,11 +14,36 @@ module.exports = {
 
 			const friendData = await User.findOneAndUpdate(filter, update, options);
 
-			// Checking if the User exists
+			// Checking if the User & friend exists
 			if (!friendData) {
 				res.status(400).json({
 					message:
 						"I can't seem to find the User you're trying to add a friend to!",
+				});
+				return;
+			}
+			res.status(200).json(friendData);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+
+	// DELETE to remove a friend from a user's friend list
+	async removeFriend(req, res) {
+		try {
+			// findOneAndUpdate method params
+			const filter = { id: req.params.id };
+			const update = { $pull: { friends: req.body.friendId } };
+			// Enforcing validators & returning updated object to user
+			const options = { runValidators: true, new: true };
+
+			const friendData = await User.findOneAndUpdate(filter, update, options);
+
+			// Checking if the User & friend exists
+			if (!friendData) {
+				res.status(400).json({
+					message:
+						"I can't seem to find the User you're trying to remove a friend from!",
 				});
 				return;
 			}
