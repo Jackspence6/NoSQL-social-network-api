39a900c6ac9702430441560c26430ab5bec346c0
Jack Spence <jackspence123456@gmail.com>
2023-12-07 14:39:44 +0200

    Added a Catch-all error Route.

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,10 @@ const apiRoutes = require("./api");
 // Middleware
 router.use("/api", apiRoutes);
 
+//  Catch-all error Route
+router.use((req, res) =>
+	res.status(404).send("404 Not Found! Please enter a valid Route path!")
+);
+
 // Exports
 module.exports = router;
