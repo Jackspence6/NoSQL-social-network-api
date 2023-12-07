8d89433f9cf2bfe906fe3d8e6ceaa6af79e0e438
Jack Spence <jackspence123456@gmail.com>
2023-12-07 12:18:13 +0200

    Added method to get all thoughts.

---
+++ controllers/thoughtController.js
@@ -0,0 +1,23 @@
+// Imports
+const Thought = require("../models/Thought");
+
+// Exports
+module.exports = {
+	// GET to get all thoughts
+	async getAllThoughts(req, res) {
+		try {
+			const thoughtData = await Thought.find({});
+
+			// Checking if Thoughts exist
+			if (!thoughtData) {
+				res
+					.status(400)
+					.json({ message: "I can't seem to find any existing Thoughts!" });
+				return;
+			}
+			res.status(200).json(thoughtData);
+		} catch (err) {
+			res.status(500).json(err);
+		}
+	},
+};
