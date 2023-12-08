1067ad49389b21d8b68d8e71e90d733b0a8870a1
Jack Spence <jackspence123456@gmail.com>
2023-12-08 15:33:29 +0200

    Added API Routing configurations for users, friends, thoughts, & reactions.

---
+++ routes/api/index.js
@@ -0,0 +1,15 @@
+// Imports
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const friendRoutes = require("./friendRoutes");
+const thoughtRoutes = require("./thoughtRoutes");
+const reactionRoutes = require("./reactionRoutes");
+
+// Middleware
+router.use("/users", userRoutes);
+router.use("/users/:id/friends", friendRoutes);
+router.use("/thoughts", thoughtRoutes);
+router.use("/thoughts/:id/reactions", reactionRoutes);
+
+// Exports
+module.exports = router;
